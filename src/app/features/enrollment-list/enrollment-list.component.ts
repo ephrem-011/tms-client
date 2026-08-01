fcc0bf36cfc501fd38823df19efdb701a58ccb57
ephrem-011 <ephraimfekadu@gmail.com>
2026-08-01 18:01:53 +0300

    M9-Session-1

--- src/app/features/enrollment-list/enrollment-list.component.ts
+++ src/app/features/enrollment-list/enrollment-list.component.ts
@@ -1,11 +1,13 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { EnrollmentStore } from '../../store/enrollment.store';
+
 @Component({
 selector: 'tms-enrollment-list',
 standalone: true,
 templateUrl: './enrollment-list.component.html'
 })
 export class EnrollmentListComponent implements OnInit {
+
 store = inject(EnrollmentStore);
 ngOnInit() {
 this.store.loadEnrollments();
