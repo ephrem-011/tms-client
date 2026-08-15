6c387f8ab135c8182aeb2045a222326b158bce97
ephrem-011 <ephraimfekadu@gmail.com>
2026-08-15 16:59:55 +0300

    M9-Session-2

--- src/app/features/enrollment-list/enrollment-list.component.spec.ts
+++ src/app/features/enrollment-list/enrollment-list.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EnrollmentListComponent } from './enrollment-list.component';
-
+// enrollment list component
 
 describe('EnrollmentListComponent', () => {
   let component: EnrollmentListComponent;
