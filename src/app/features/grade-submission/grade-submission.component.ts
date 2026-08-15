acf72f5fa6c38d17bd4cc863d8ddf16f456614cd
ephrem-011 <ephraimfekadu@gmail.com>
2026-08-15 18:36:44 +0300

    M9-Session-3

---
+++ src/app/features/grade-submission/grade-submission.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grade-submission',
+  imports: [],
+  templateUrl: './grade-submission.component.html',
+  styleUrl: './grade-submission.component.scss',
+})
+export class GradeSubmissionComponent {}
